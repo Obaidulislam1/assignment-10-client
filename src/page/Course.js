7ffe4383565430d400a5e3c61eea6c407814ff98
obaidul islam1 <mursalinislam537215@gmail.com>
2022-10-26 12:44:51 +0600

    course showing complete

--- src/page/Course.js
+++ src/page/Course.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Course = ({ course }) => {
     console.log(course)
@@ -7,11 +8,16 @@ const Course = ({ course }) => {
     } = course;
     return (
         <div>
-            <div className="card card-compact w-96 bg-base-100 shadow-xl ">
-                <figure><img className='w-50 h-40' src={image} alt="Shoes" /></figure>
+        <div className="card card-compact w-full bg-base-100 shadow-xl">
+                <figure><img className='w-full h-48' src={image} alt="Shoes" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{title}</h2>
+                    <p>{
+                    description.length > 100 ?
+                    <p>{description.slice(0,100) + '...'} <Link>Read more</Link></p>:
                     <p>{description}</p>
+
+                    }</p>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Details</button>
                     </div>
--- src/page/Courses.js
+++ src/page/Courses.js
@@ -6,13 +6,20 @@ const Courses = () => {
     const courses = useLoaderData();
     console.log(courses);
     return (
-        <div className='flex'>
-           <div className='w-1/3'>
-            <h1>course name section</h1>
+        <div className='md:flex w-10/12 m-auto'>
+           <div className='w-1/4 m-4'>
+            <h1>Digital marketing</h1>
+            <h1>Graphics Design</h1>
+            <h1>PHP Language</h1>
+            <h1>Jquery Click</h1>
+            <h1>Al Quran</h1>
+            <h1>Robotics</h1>
            </div>
-           {
+          <div className='grid sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10'>
+          {
             courses.map(course =><Course key={course.id} course={course}></Course>)
            }
+          </div>
         </div>
     );
 };
