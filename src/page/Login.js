3f1571c36860dda2a0e318d1d43fb0df4820124d
obaidul islam1 <mursalinislam537215@gmail.com>
2022-10-26 21:51:43 +0600

    login page complete

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Sharefile/authProvider/AuthProvider';
 
 const Login = () => {
+    const navigate = useNavigate();
+    const { login } = useContext(AuthContext)
+    const loginIn = event => {
+        event.preventDefault()
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        login(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                navigate('/')
+                form.reset();
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
     return (
-        <form className="form-control w-full max-w-xs m-auto">
+        <form onSubmit={loginIn} className="form-control w-full max-w-xs m-auto">
             <label className="label">
                 <span className="label-text">Your email eddress</span>
             </label>
-            <input type="email" placeholder="your email eddress" name='email' className="input input-bordered w-full max-w-xs" required/>
+            <input type="email" placeholder="your email eddress" name='email' className="input input-bordered w-full max-w-xs" required />
             <label className="label">
                 <span className="label-text">Password</span>
             </label>
-            <input type="password" name='password' placeholder="Type here password" className="input input-bordered w-full max-w-xs" required/>
+            <input type="password" name='password' placeholder="Type here password" className="input input-bordered w-full max-w-xs" required />
             <button type='submit' className="btn btn-active btn-ghost mt-5">Login</button>
         </form>
     );
--- src/page/Registation.js
+++ src/page/Registation.js
@@ -1,10 +1,12 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../Sharefile/authProvider/AuthProvider';
 import { GoogleAuthProvider } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 
 
 const Registation = () => {
+    const  navigate = useNavigate();
 
     const {googleSign,register} = useContext(AuthContext)
 
@@ -15,26 +17,27 @@ const Registation = () => {
 
         .then(result => {
             const user = result.user;
-            console.log(user)
+            console.log(user);
         })
         .catch(error =>{
             console.error(error)
         })
     }
 
-const formSubmit = event =>{
+const fromSubmit = event =>{
     event.preventDefault()
-    const form =event.target;
-    const name = form.name.value;
-    const email = form.email.value;
-    const password = form.password.value;
-    const confirmPassword = form.confirmPassword.value;
-    const photo = form.photo.value;
+    const from =event.target;
+    const name = from.name.value;
+    const email = from.email.value;
+    const password = from.password.value;
+    const confirmPassword = from.confirmPassword.value;
+    const photo = from.photo.value;
     console.log(name,email,password,confirmPassword,photo);
     register(email,password)
     .then(result =>{
       const user = result.user;
       console.log(user);  
+      navigate('/login')
     })
     .catch(error =>{
         console.error(error);
@@ -43,7 +46,7 @@ const formSubmit = event =>{
 
     return (
        <div className='mt-20'>
-         <form onSubmit={formSubmit} className="form-control w-full max-w-xs m-auto">
+         <from onSubmit={fromSubmit} className="from-control w-full max-w-xs m-auto">
             <label className="label">
                 <span className="label-text">What is your name?</span>
             </label>
@@ -65,7 +68,7 @@ const formSubmit = event =>{
             </label>
             <input type="password" name='confirmPassword' placeholder="Confirm password" className="input input-bordered w-full max-w-xs" required/>
             <button type='submit' className="btn btn-active btn-ghost mt-5">Register</button>
-        </form>
+        </from>
 
         <div className='flex justify-center'>
             <button onClick={handleGoogle}> 
